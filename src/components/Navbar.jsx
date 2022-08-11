1af55f0961c9ab8861f89f52099c9527b65f0319
eliezerpolidor <eliezerpolidor69@gmail.com>
2022-08-10 23:32:57 -0400

    cargando proyecto Reactjs-REdux-Toolikit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+    return ( 
+        <nav className = "navbar navbar-dark bg-info display-1" >
+            <div className = "container" >
+                <h1 className = "navbar-brand text-center text-white" > Probando Código React Redux Toolkit / El EndPoint lo tome de esta App la cual es gratuita: https: //reqres.in/</h1>
+            </div> 
+        </nav>
+    )
+};
+
+export default Navbar;
